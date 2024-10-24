02b9ca7a43685edde554f7e041985cca94e8a6fd
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-10-24 23:33:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+// const getSumBtn = document.createElement("button");
+// getSumBtn.append("Get Total Price");
+// document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
-  
-};
+function calculateTotal()
+{
+ const priceElements = document.querySelectorAll(".price");
+ let total = 0;
+priceElements.forEach((priceElement) => {
+          total += parseFloat(priceElement.textContent);
+        });
+ const table=document.getElementById("main-table");
+	const row=document.createElement("tr");
+	const cell=document.createElement("td");
+	cell.colSpan=2;
+	cell.textContent="Total price is Rs"+total;
+	row.appendChild(cell);
+	table.appendChild(row);
+}
 
-getSumBtn.addEventListener("click", getSum);
+//getSumBtn.addEventListener("click", getSum);
 
